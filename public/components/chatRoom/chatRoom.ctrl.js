354e45bde6f9839fa38ad8c034d281e5b2a54004
Harald Lønsethagen <haraldlons@gmail.com>
2018-10-04 15:00:44 +0200

    cleanup chatRoomCtrl

--- public/components/chatRoom/chatRoom.ctrl.js
+++ public/components/chatRoom/chatRoom.ctrl.js
@@ -3,77 +3,61 @@
 
 
 app.controller("chatRoomCtrl", ["$scope","Auth", "$state", "$rootScope", "chatMessages", function($scope, Auth, $state, $rootScope, chatMessages){
+    console.log("chatRoomCtrl loaded");
+    var vm = this; // Capture variable
 
-	/*Capture variable*/
-	var vm = this;
-console.log("chatRoomCtrl loaded");
+    // Initialize Variables from Factories
+    vm.auth = Auth;
+    vm.messages = chatMessages;
+    // Initialize Variables
+    vm.message = "";
+    vm.random_0_to_100 = Math.floor(Math.random()*100); // Number from 0 to 99
+    vm.isLoggedInOrNot = "Siden du ikke er logget inn vil du vises som 'Guest: " +  vm.random_0_to_100+"'";
 
-//HELLO
-	/*Defines all variables used*/
-vm.auth = Auth;
-vm.message;
-vm.messages;
-vm.random_0_to_100 = Math.floor(Math.random()*100); //Number from 0 to 99
-// console.log("Random tall: " + vm.random_0_to_100) 
-vm.isLoggedInOrNot = "Siden du ikke er logget inn vil du vises som 'Guest: " +  vm.random_0_to_100+"'";
-vm.message = "";
-	/*Defines all functions used*/
-vm.addMessage = addMessage;
+    // Functions
+    vm.addMessage = addMessage;
 
 
+    //For $broadcast til alle kontrollere må man bruk $rootScope(husk å inkludere som parameter til funksjonen)
+    $rootScope.$broadcast('navBarUpdate', "chat");
 
-vm.auth.$onAuthStateChanged(function(firebaseUser){
-	if(firebaseUser){
-        if(firebaseUser.displayName){
-          vm.userName = firebaseUser.displayName;
+
+    vm.auth.$onAuthStateChanged(function(firebaseUser){
+        if(firebaseUser){
+            if(firebaseUser.displayName){
+                vm.userName = firebaseUser.displayName;
+            }else {
+                vm.userName = firebaseUser.email;
+            }
+            console.log("User: ", vm.userName);
+
+            vm.isLoggedInOrNot = "Du vil vises som '" + vm.userName + "'";
         }else {
-          vm.userName = firebaseUser.email;
+            if(!vm.userName){
+                vm.userName = "Guest "+ vm.random_0_to_100;
+            }
+            $rootScope.$broadcast('isLogged', "chat");
         }
-        console.log("User: ", vm.userName);
-
-        vm.isLoggedInOrNot = "Du vil vises som '" + vm.userName + "'";
-      }else {
-      	if(!vm.userName){
-          vm.userName = "Guest "+ vm.random_0_to_100;
-      	}
-      	$rootScope.$broadcast('isLogged', "chat");
-
-      }
-});
-
- 
-//For $broadcast til alle kontrollere må man bruk $rootScope(husk å inkludere som parameter til funksjonen) 
-$rootScope.$broadcast('navBarUpdate', "chat");
-
-
-	/*Define firebaseArray things to use the chat*/
-
-	vm.messages = chatMessages;
-
-	function addMessage(){
-		vm.messages.$add({
-			from: vm.userName,
-			content: vm.message,
-		})
-		vm.message = "";
-	}
-
-	/*If no messages in the list makes on entry*/
-vm.messages.$loaded(function() {
-
-
-  if (vm.messages.length === 0) {
-    vm.messages.$add({
-      from: "Firebase Docs",
-      content: "Hello world!"
     });
-  }
-});
-
-	/*End firebaseArray chat thingy*/
 
 
+    function addMessage(){
+        vm.messages.$add({
+            from: vm.userName,
+            content: vm.message,
+        });
+        vm.message = "";
+    }
+
+
+    /*If no messages in the list makes on entry*/
+    vm.messages.$loaded(function() {
+        if (vm.messages.length === 0) {
+            vm.messages.$add({
+            from: "Firebase Docs",
+            content: "Hello world!"
+            });
+        }
+    });
 }]);/*End chatRoomCtrl*/
-
-
 })(); /*End function thingy*/
