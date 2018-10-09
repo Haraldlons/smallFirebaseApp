bcd947fe1d81b80804bae6de76e892e922133efe
Harald Lønsethagen <haraldlons@gmail.com>
2018-10-09 19:16:54 +0200

    add alot of input fields

--- public/components/main.ctrl.js
+++ public/components/main.ctrl.js
@@ -1,63 +1,39 @@
 (function(){
 	'use strict';
 
-	//Trenger ikke initialisere firebase på bytt igjen siden vi har gjort det i app.js
-	//kan da bruke rett fram 
-	//var ref = firebase.database().ref();
-
-app.controller("mainController", ["$scope","$mdToast", "$mdDialog", "$state", function($scope,$mdToast,$mdDialog,$state){
+app.controller("mainController", ["$scope","$mdToast", "$mdDialog", "$state", "$http", function($scope, $mdToast, $mdDialog, $state, $http){
     console.log("mainController loaded");
-	/*Dette er en såkalt 'Capture Variable'. Kan bruke vm i steden for $scope*/
-	var vm = this;
+    var vm = this; // Capture variable
 
-	/*Definerer alle variabler brukt*/
-	vm.currentNavItem;
-	vm.message;
-  vm.nyMelding;
-  vm.isLoggedIn = false;
-	vm.showAccount = true;
-  vm.currentNavItem = 'homeName';
-	vm.message = "Melding fra mainController";
-  vm.nyMelding = "Dette er en ny melding fra HomeCtrl"
-  /*Definerer alle funksjoner brukt*/
-  vm.showToast = showToast;
-
-
-	
-  /*Blir alt for mange Toast'er*/
-// vm.auth.$getAuth(function(firebaseUser){
-//   if(firebaseUser){
-//     showToast("Du er logget!\nfirebaseUser.email");
-//   }else{
-//     showToast("Du er ikke logget inn");
-//   }
-// })
-
-  /*When we go to different states we need to update the red line under the tabs*/
-  /*Cause this wil not persist during reload of page*/
-  // $scope.$on("navBarUpdate", function(event, data){
-  //     // console.log("$on detected data: ", data);
-  //     vm.currentNavItem = data;
-  //   });
-  // $scope.$on("showToast", function(event, data){
-  //   showToast(data);
-  //   console.log("Data showToast: ", data);
-  // })
+    // Variables
+    vm.system_id = "test";
+    vm.education_choice = "University qualifying examination";
+    vm.ethnicity = "White";
+    vm.age = 24;
+    vm.gender = "Male";
+    vm.workoutTime = "30 minutes or more";
+    vm.workoutFrequency = "Once a week";
+    vm.hardness = "Little hard breathing and sweating";
+    vm.restingPulse = 50;
+    vm.maximumHeartRate = 190;
+    vm.weight = 80;
+    vm.height = 180;
+    vm.isLoggedIn = false; // Is used in header
+    vm.currentNavItem = 'homeName'; // Is used in header
 
 
-
-
-	function showToast(message){
+	vm.showToast = function showToast(message){
 		$mdToast.show(
 			$mdToast.simple()
 					.content(message)
 					.position('top, right')
-					.hideDelay(1000)
+					.hideDelay(5000)
 			);
-	}
+	};
+
 
 	/*Start Icon greier*/
- var originatorEv;
+    var originatorEv;
 
     vm.openMenu = function($mdOpenMenu, ev) {
       originatorEv = ev;
@@ -83,29 +59,252 @@ app.controller("mainController", ["$scope","$mdToast", "$mdDialog", "$state", fu
       originatorEv = null;
     };
 
-    vm.checkVoicemail = function() {
-      // vm never happens.
+
+    function testGetResponseAPI() {
+        const proxyurl = "https://cors-anywhere.herokuapp.com/"; // https://tinyurl.com/y9p4bfl6
+        const url = 'https://www.worldfitnesslevel.org/service/api/response';
+        const data = {
+            "userId": "asdf",
+            "systemId": vm.system_id,
+            "responseId": "",             //must be used if you want to update an existing response, or else remove or set blank
+            "input": {
+                "education": 12,            //see /types
+                "ethnicity": 1,             //see /types
+                "gender": 1,                //see /types
+                "workoutFrequency": 2,      //see /types
+                "workoutTime": 2,           //see /types
+                "hardness": 3,              //see /types
+                "restingPulse": 45,         //beats pr minute
+                "maximumHeartRate": 189,    //beats pr minute
+                "weight": 83,               //in kilograms
+                "height": 187,              //in centimeters
+                "age": 37,
+                "latitude": 63.4305,        //optional
+                "longitude": 10.3951,       //optional
+                "city": "Trondheim",        //optional
+                "country": "Norway",        //optional
+                "group": ""                 //optional - only use group in agreement with API provider
+            }
+        };
+
+        $http({
+            url: proxyurl + url,
+            method: 'POST',
+            data: data,
+            headers: {'Content-Type': 'application/json'},
+        }).then(function (data) {
+            console.log(data);
+        }).catch(function (data, status, headers, config) {
+            //todo: report to user
+            console.log("Error while doing");
+            $log.error({
+                data: data,
+                status: status,
+                headers: headers,
+                config: config
+            });
+        });
+    }
+
+
+    vm.callAPI = function () {
+        console.log("Calling API with current variables");
+        vm.input_string = parseInt(vm.input_string) + 1;
+        // testGetResponseAPI();
     };
-	/*Slutt Icon greier*/
 
+
+    vm.test_dialog = function changePassPrompt(event){
+        var confirm = $mdDialog.confirm()
+            .title('Bytt passord')
+            .textContent("Trykk på 'Send email' for å endre passord." + vm.education_choice + " Vi vil da sende en mail til '"+ "deg" + "' med instruksjoner om hvordan du endrer passord.")
+            .ariaLabel('deletePictureURL')
+            // .initialValue('Buddy')
+            .targetEvent(event)
+            .ok('Send email')
+            .cancel('Ikke endre');
+
+        $mdDialog.show(confirm)
+            .then(function() {
+                /*User pressed OK*/
+                /*result wil be what user inputted*/
+                console.log("You pressed OK");
+                showToast("You pressed OK");
+                // var updates = {};
+                //  updates["/users/"+uid+"/bilde"] = "";
+                //  rootRef.update(updates);
+
+                // showAlert("Profilbilde slettet", "Du slettet akkurat profilbildet ditt. Finn gjerne et annet bilde")
+            }, function() {
+                /*User pressed cancel*/
+                console.log("You pressed CANCEL");
+                showToast("You pressed CANCEL");
+                // showToast("Du endret ikke passordet")
+            });
+    };
+
+    vm.printAllData = function(){
+        console.log()
+    };
+
+    vm.valueOptions = {
+        education: [
+            {
+                id: 1,
+                value: "1",
+                text: "Primary school 7-10 years"
+            },
+            {
+                id: 2,
+                value: "2",
+                text: "Continuation school"
+            },
+            {
+                id: 3,
+                value: "3",
+                text: "Folk high school"
+            },
+            {
+                id: 4,
+                value: "4",
+                text: "High school"
+            },
+            {
+                id: 5,
+                value: "5",
+                text: "Intermediate school"
+            },
+            {
+                id: 6,
+                value: "6",
+                text: "Vocational school"
+            },
+            {
+                id: 7,
+                value: "7",
+                text: "1-2 years high school"
+            },
+            {
+                id: 8,
+                value: "8",
+                text: "University qualifying examination"
+            },
+            {
+                id: 9,
+                value: "9",
+                text: "Junior college"
+            },
+            {
+                id: 10,
+                value: "10",
+                text: "A levels University or other post-secondary education"
+            },
+            {
+                id: 11,
+                value: "11",
+                text: "Less than 4 years University/college"
+            },
+            {
+                id: 12,
+                value: "12",
+                text: "4 years or more University/college"
+            }
+        ],
+        ethnicity: [
+            {
+                id: 1,
+                value: "white",
+                text: "White"
+            },
+            {
+                id: 2,
+                value: "hispanic_or_latino",
+                text: "Hispanic or Latino"
+            },
+            {
+                id: 3,
+                value: "black_or_african_american",
+                text: "Black or African American"
+            },
+            {
+                id: 4,
+                value: "native_american_or_american_indian",
+                text: "Native American or American Indian"
+            },
+            {
+                id: 5,
+                value: "asian_pacific_islander",
+                text: "Asian / Pacific Islander"
+            },
+            {
+                id: 6,
+                value: "other",
+                text: "Other"
+            }
+        ],
+        gender: [
+            {
+                id: 1,
+                value: "MALE",
+                text: "Male"
+            },
+            {
+                id: 2,
+                value: "FEMALE",
+                text: "Female"
+            }
+        ],
+        workoutFrequency: [
+            {
+                id: 1,
+                value: 0,
+                text: "Almost never or less than once a week"
+            },
+            {
+                id: 2,
+                value: 1,
+                text: "Once a week"
+            },
+            {
+                id: 3,
+                value: 2,
+                text: "2-3 times a week"
+            },
+            {
+                id: 4,
+                value: 3,
+                text: "Almost every day"
+            }
+        ],
+        workoutTime: [
+            {
+                id: 1,
+                value: 1,
+                text: "Under 30 min"
+            },
+            {
+                id: 2,
+                value: 1.5,
+                text: "30 minutes or more"
+            }
+        ],
+        hardness: [
+            {
+                id: 1,
+                value: 1,
+                text: "I take it easy without breathing hard or sweating"
+            },
+            {
+                id: 2,
+                value: 5,
+                text: "Little hard breathing and sweating"
+            },
+            {
+                id: 3,
+                value: 10,
+                text: "I go all out"
+            }
+        ]
+    };
 }]); /*End mainController*/
-
-
-// app.controller("AccountCtrl", ["currentAuth","$scope", function(currentAuth,$scope) {
-//   // currentAuth (provided by resolve) will contain the
-//   // authenticated user or null if not signed in
-
-//   Capture variable, vm= view model, bare enklere å bruke enn $scope
-//   var vm = vm;
-  
-//   /*Definerer variabler brukt*/
-//   vm.message = "Melding fra AccountCtrl";
-//   vm.currentNavItem = 'accountName';
-//   /*Definerer funksjoner brukt*/
-
-
-// }]);
-
-
-
 })(); /*End hele shiten*/
